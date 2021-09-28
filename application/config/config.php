06f643e3b70c90ff378fdff3615d54d0d0980ade
Gloria-CHEN4 <69610282+Gloria-CHEN4@users.noreply.github.com>
2021-09-28 13:15:13 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/infs3208';
+$config['base_url'] = 'http://'.$_SEVER['HTTP_HOST'].'/';
 
 /*
 |--------------------------------------------------------------------------
